e4d48a6a41a950346350d4584ce7330722c910da
Savy011 <106910589+Savy011@users.noreply.github.com>
2023-06-10 20:08:15 +0530

    Finished Task 13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -20,11 +20,37 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/a:id', blogFinder, async (req, res) => {
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+});
+
+router.delete('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
     await req.blog.destroy()
   }
   res.sendStatus(204);
 });
+
+router.put('/:id', blogFinder, async (req, res) => {
+  const body = req.body
+  if (req.blog) {
+    try {
+      const updatedBlog = await req.blog.update(
+        { likes: body.likes },
+        { where: { id: req.params.id } }
+      )
+
+      res.status(200).json(updatedBlog)
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  } else {
+    res.status(404).end()
+  }
+})
 
 module.exports = router
